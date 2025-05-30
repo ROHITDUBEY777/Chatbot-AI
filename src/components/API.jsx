c604b0d2497068ddd24b395aab67fa67898f9d94
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-05-31 01:18:40 +0530

    fifth commit

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -10,15 +10,18 @@ const API = () => {
   const[input ,setinput] = useState(" ");
   const[reply,setreply] = useState( " ");
   const[loading,setLoading] = useState( false ); 
+  
 
 
 
   const handlesubmit = async (e) => {
-  e.preventDefault()
+  e.preventDefault();
    setLoading(true);
    if(loading) return  LoadingThreeDotsJumping()
    const res = await askGemini(input);
+ 
    setreply(res);
+   
    setLoading(false);
 
   }
@@ -75,22 +78,26 @@ return response.data.candidates[0].content.parts[0].text;
 }
 
   return (
-    <div className='flex flex-col  text-white rounded-xl px-16 py-6 w-[41rem]  items-center justify-center bg-[#7e22ce]'>
+    <div className='flex flex-row justify-center min-h-screen '>
+      
+    <div className='flex flex-col  w-2/3  h-fit   my-10     text-white rounded-xl    px-16 py-10  bg-[#57534e]'>
       {reply && (
-        <div className='mt-[4vh] p-4 bg-[#7e22ce] rounded shadow '>
-          <h2 className='text-lg font-bold text-black  mb-2 ' >Gemini's Response : </h2>
-          <p className='text-black'>{reply}</p>
+        <div className='mt-[4vh] p-4bg-[#57534e] rounded shadow '>
+          <h1 className='text-xl mb-2'>Ask anything, we’ve got answers.</h1>
+          <h2 className='text-base font-bold text-white  mb-2 ' > Response : </h2>
+          <p className='text-white text-sm py-4 '>{reply}</p>
           </div>
       )}
-      <div className='flex flex-row  justify-between '>
+      <div className='flex flex-col md:flex-row lg:flex-row xl:flex-row justify-between '>
 
       <input type="text"  placeholder='Ask a question ' 
         value={input}
         onChange={(e)=> setinput(e.target.value)}
-        className='w-full text-black border bg-[#7e22ce] px-4 py-2 '  />
-        <motion.button type='button' whileHover={{scale:1.1}}  onClick={handlesubmit} className='bg-blue-600 border text-black w-1/3 '> {loading ? "Thinking....." : "Search"}  </motion.button>
+        className='w-full text-white border bg-[#334155] mt-[2vh] px-4 py-2 '  />
+        <motion.button type='button' whileTap={{scale:1.1}}  onClick={handlesubmit} className='mt-[2vh] bg-blue-600 border text-white px-3  w-fit md:w-1/3 '> {loading ? "Thinking....." : "Search"}  </motion.button>
         </div>
     </div>
+        </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdMenu } from "react-icons/io";
 
 import {motion} from 'motion/react';
 
 const Navbar = () => {
+  const[open,setopen] = useState(false);
   return (
     <div>
      <nav className='flex flex-row  text-white top-0 justify-center sticky backdrop-blur-3xl z-50  px-3  py-5   w-full '>
@@ -13,25 +14,26 @@ const Navbar = () => {
 
            
             <li className='cursor-normal'>Chatbot AI</li>
-            <div className='text-2xl   flex-row md:flex   md:gap-16 lg:gap-18 xl:gap-22 hidden justify-center'>
+            <div className='text-2xl   flex-row md:flex md:text-2xl   md:gap-16 lg:gap-18 xl:gap-22 hidden justify-center'>
 
-            <a href="#Hero"><li className='cursor-pointer'>Home </li></a>
-            <li className='cursor-pointer'>About </li>
-            <li className='cursor-pointer'>Service </li>
-            <li className='cursor-pointer'>Contact Us  </li>
+            <a href="#Hero"><li className='cursor-pointer opacity-70 hover:opacity-100'>Home </li></a>
+            <li className='cursor-pointer opacity-70 hover:opacity-100'>About </li>
+            <li className='cursor-pointer opacity-70 hover:opacity-100'>Service </li>
+            <li className='cursor-pointer opacity-70 hover:opacity-100'>Contact Us  </li>
             </div>
           <div className='flex gap-2'>
 
- <motion.button  whileHover={{scale:0.7}} whileTap={{scale:1.2 }} className='md:text-xl lg:text-xl xl:text-xl hidden md:flex  border cursor-pointer   text-white px-3 py-2 rounded-lg '>
+ <motion.button  whileHover={{scale:0.7}} whileTap={{scale:1.2 }} className='md:text-xl lg:text-xl xl:text-xl hidden md:flex opacity-70 hover:opacity-100 border cursor-pointer   text-white px-3 py-2 rounded-lg '>
           
                  sign-in
                 </motion.button> 
-          <motion.button  whileHover={{scale:0.7}} whileTap={{scale:1.2 }} className='md:text-xl lg:text-xl xl:text-xl hidden md:flex   border cursor-pointer   text-white px-3 py-2 rounded-lg '>
+          <motion.button  whileHover={{scale:0.7}} whileTap={{scale:1.2 }} className='md:text-xl lg:text-xl xl:text-xl hidden md:flex  opacity-70 hover:opacity-100 border cursor-pointer   text-white px-3 py-2 rounded-lg '>
           
                  Get Started
                 </motion.button>  
 
-            <IoMdMenu className='cursor-pointer scale-200 xl:hidden lg:hidden md:hidden flex' />
+            <IoMdMenu 
+             className='cursor-pointer scale-200 xl:hidden lg:hidden md:hidden flex' />
 
           </div>
           
